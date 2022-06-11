a79a0a81f9a6e4624c223b68802e6a21292aef6a
Derek <derekbelloni10@gmail.com>
2022-06-11 11:25:43 -0600

    postman get is grabbing empty array

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
 
+
+
 const goalSchema = new mongoose.Schema({
   text: {
     type: String,
     required: true
   }
+}, {
+  timestamps: true
 })
+
+module.exports = mongoose.model('Goal', goalSchema)
